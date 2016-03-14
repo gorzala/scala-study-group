5ddbdf42268b0bea211c7f9f2491981d7e4aa9cd
Marc Gorzala <marc.gorzala@zalando.de>
2016-03-14 13:09:38 +0100

    initial commit
    - small code snippets for the first chapter

---
+++ code/src/main/scala/ssg/cafe/Cafe.scala
@@ -0,0 +1,36 @@
+package ssg.cafe
+
+class Cafe {
+  def buyCoffee(cc: CreditCard): (Coffee, Charge) = {
+    val cup = Coffee(1.2)
+    (cup, Charge(cc, cup.price))
+  }
+
+  def buyCoffees(cc: CreditCard, n: Int): (List[Coffee], Charge) = {
+    val purchases: List[(Coffee, Charge)] = List.fill(n)(buyCoffee(cc))
+    val (coffees, charges) = purchases.unzip
+    (coffees, charges.reduce( (c1, c2) => c1.combine(c2)))
+  }
+
+  def coalesce(charges: List[Charge]) : List[Charge] =
+    charges.groupBy(_.cc).values.map(_.reduce( (x, y) => x combine y)).toList
+}
+
+case class Coffee(price: Double)
+
+case class CreditCard(owner: String)
+
+case class Charge(cc: CreditCard, amount: Double) {
+  def combine(other: Charge): Charge =
+    if (cc == other.cc)
+      Charge(cc, amount + other.amount)
+    else
+      throw new Exception("Can't combine charges to different cards")
+}
+
+object Question {
+  def today: java.util.Date = {
+    new java.util.Date
+  }
+}
+
---
+++ code/src/main/scala/ssg/cafe/Worksheet.sc
@@ -0,0 +1,36 @@
+import ssg.cafe._
+
+val myCard = CreditCard("Marc")
+val myCafeBar = new Cafe
+
+val myPurchase = myCafeBar.buyCoffee(myCard)
+
+val myPurchases = myCafeBar.buyCoffees(myCard, 4)
+
+val x = Question.today
+
+
+
+
+
+
+
+
+
+val listOfTuples : List[(Char,Int)] = List(('a',1),('b',2),('c',3))
+
+val (chars, numbers) = listOfTuples.unzip
+val amexCC = CreditCard("amex")
+val amexCharge1 = Charge(amexCC, 3.4)
+val amexCharge2 = Charge(amexCC, 2.6)
+val amexCharges = List(amexCharge1, amexCharge2)
+val visaCC = CreditCard("visa")
+val visaCharge1 = Charge(visaCC, 7)
+val visaCharges = List(visaCharge1)
+val totalCharges = visaCharges ::: amexCharges
+
+val coalescedCharges = myCafeBar.coalesce(totalCharges)
+
+val x = "Hello, World"
+
+val r1 = x.
