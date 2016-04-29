fe49cbce3e4e742b18242d5363cfa8dd181fde0c
Kai Moritz <kai@juplo.de>
2016-04-29 21:34:32 +0200

    Implemented 5.1

--- exercises/5.scala
+++ exercises/5.scala
@@ -3,6 +3,12 @@ sealed trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => Some(h())
   }
+
+  /** 5.1 */
+  def toList: List[A] = this match {
+    case Cons(h,t) => h() :: t().toList
+    case Empty => Nil:List[A]
+  }
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
@@ -17,4 +23,13 @@ object Stream {
 
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+}
+
+
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+    println("Stream(1,2,3,5).toList: " + Stream(1,2,3,5).toList);
+  }
 }
