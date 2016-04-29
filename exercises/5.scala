3564a858467e6f76029e691803857046ee9bfe4b
Kai Moritz <kai@juplo.de>
2016-04-29 21:20:34 +0200

    Addes a helper-function defined later on

--- exercises/5.scala
+++ exercises/5.scala
@@ -1,4 +1,9 @@
-sealed trait Stream[+A]
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => Some(h())
+  }
+}
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
