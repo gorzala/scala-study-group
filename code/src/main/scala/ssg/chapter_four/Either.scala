91c215a4afdc39ba794660e8a1ce07d5dabfefc3
Marc Gorzala <marc.gorzala@zalando.de>
2016-05-01 18:05:48 +0200

    Excercise 4.6

--- code/src/main/scala/ssg/chapter_four/Either.scala
+++ code/src/main/scala/ssg/chapter_four/Either.scala
@@ -1,10 +1,42 @@
 package ssg.chapter_four
 
-sealed trait Either[+E, +A]
+sealed trait Either[+E, +A] {
+
+  def map[B](f: A => B): Either[E, B] = {
+    this match {
+      case Left(a)  => Left(a)
+      case Right(a) => Right(f(a))
+    }
+  }
+
+  def flatMap[EE >: E, B](f: A => Either[EE,B]): Either[EE, B] = {
+    this match {
+      case Left(e)  => Left(e)
+      case Right(e) => f(e)
+    }
+  }
+
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = {
+    this match {
+      case Left(e) => b
+      case Right(e) => Right(e)
+    }
+  }
+
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = {
+    for {
+      aa <- this
+      bb <- b
+    } yield(f(aa,bb))
+  }
+
+}
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
 object Either {
+
+
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = {
     if (xs.isEmpty)
       Left("mean of an empty List!")
--- code/src/main/scala/ssg/chapter_four/InsuranceQuote.scala
+++ code/src/main/scala/ssg/chapter_four/InsuranceQuote.scala
@@ -1,17 +1,23 @@
 package ssg.chapter_four
 
-import Option.Try
-import Option.map2
 
 object InsuranceQuote {
+
   def insuranceRateQuote(age: Int, numberOfSpeedingTickets: Int): Double = {
     (age * numberOfSpeedingTickets).toDouble
   }
 
   def parseInsuranceRateQuote(age: String, numberOfSpeedingTickets: String): Option[Double] = {
-    val optAge = Try(age.toInt)
-    val optTickets = Try(numberOfSpeedingTickets.toInt)
+    val optAge = Option.Try(age.toInt)
+    val optTickets = Option.Try(numberOfSpeedingTickets.toInt)
 
-    map2(optAge, optTickets)(insuranceRateQuote)
+    Option.map2(optAge, optTickets)(insuranceRateQuote)
+  }
+
+  def parseWithEither(age: String, numberOfTickets: String): Either[Exception, Double] = {
+    val optAge = Either.Try(age.toInt)
+    val optTickets = Either.Try(numberOfTickets.toInt)
+
+    optAge.map2(optTickets)(insuranceRateQuote)
   }
 }
